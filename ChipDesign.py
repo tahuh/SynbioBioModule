f411390fa0851cb1de3a0111d939abe3bb45d1a1
tahuh <tahuh1124@gmail.com>
2017-03-13 19:08:12 +0900

    Update ChipDesign.py

--- ChipDesign.py
+++ ChipDesign.py
@@ -2,10 +2,6 @@
 
 """
 ChipDesign.py
-
-code availity : Prof. Bang's members ( Synthetic biology lab. Yonsei Univ. Korea )
-
-Author : Sunghoon Heo
 """
 
 from argparse import ArgumentParser
@@ -13,13 +9,13 @@ from Designer import Designer
 
 parser = ArgumentParser()
 
-parser.add_argument("--left_flank" , type=str, required=True, help= "Left flanking sequence")
-parser.add_argument("--right_flank" , type=str, required=True, help= "Right flanking sequence")
+parser.add_argument("--left_flank" , type=str, required=False, help= "Left flanking sequence")
+parser.add_argument("--right_flank" , type=str, required=False, help= "Right flanking sequence")
 parser.add_argument("--left_insert" , type=str, required=True, help= "Left insert sequence")
 parser.add_argument("--right_insert" , type=str, required=True, help= "Right insert sequence")
 parser.add_argument("--faname" , type=str, required=True, help = "FASTA name with full path")
 parser.add_argument("--oname" , type=str, required=True, help="Output name with full path")
-parser.add_argument("--freq" , type=float, required=False, help="freq cut for human codon usage" , default=0.10)
+parser.add_argument("--frq" , type=float, required=False, help="freq cut for human codon usage" , default=0.10)
 
 parser.add_argument("--tile_cov",type=int , required = False, help= "Tile coverage such as 10 ,.." , default=10)
 
@@ -30,7 +26,7 @@ parser.add_argument("--chip_right" , type=str, required=False, help="Right addit
 
 opts = parser.parse_args()
 
-designer = Design(opts)
+designer = Designer(opts)
 
 optdict = vars(opts)
 
@@ -39,9 +35,9 @@ print "Designer starts to design"
 designer.design()
 
 ofile = open(opts.oname, "w")
-for id , ret_set in designer.records:
+for id , ret_set in designer.result:
 	length = len(ret_set)
-	for k in xrange(len(length)) :
+	for k in xrange(length) :
 		ofile.write(id + "_" + str(k+1) + "\t" + ret_set[k] + "\n")
 		
 ofile.close()
