3f8d6af64aacc07221da1049882f255686848af9
tahuh <tahuh1124@gmail.com>
2018-03-23 14:48:12 +0900

    Create count_num_seqs.py

---
+++ count_num_seqs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import argparse
+import sys
+parser = argparse.ArgumentParser()
+parser.add_argument("--infile", type=str, required= True, help="infile")
+parser.add_argument("--type", type=str,default="fasta", help="infile type(fasta/fastq) default=fasta)
+
+if args.type == 'fasta':
+    head = '>'
+elif args.type == 'fastq':
+    head = '@'
+else:
+    raise AttributeError("--type argument must be fasta for fastq. others are not accepted\n")
+cnt = 0
+with open(args.infile) as F:
+    for line in F:
+        if line[0] == head:
+            cnt += 1
+sys.stdout.write("File %s contains %d sequences\n"%(args.infile , cnt))
